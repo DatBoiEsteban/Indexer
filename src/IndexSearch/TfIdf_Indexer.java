161ffbdc9146000b4552f7c330a7bd66cfb3c9e9
Esteban Mata <esteban1113001@gmail.com>
2018-09-18 22:44:58 -0600

    Fixed TF-IDF

--- src/IndexSearch/TfIdf_Indexer.java
+++ src/IndexSearch/TfIdf_Indexer.java
@@ -1,136 +1,153 @@
 package IndexSearch;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 import fileManager.FileFather;
 
-public class TfIdf_Indexer implements I_Indexer{
-	private Map<String, Double> Idfdict ;
-	private  Set<String> WordSet; 
+public class TfIdf_Indexer extends Thread implements I_Indexer {
+	private Map<String, Double> Idfdict = new HashMap<String, Double>();
+	private Set<String> WordSet = new HashSet<String>();
 	private ArrayList<FileFather> FileList;
-	private  Object Index[][];
-	
-	//CONSTRUCTOR 
+	private Object Index[][];
+	private Boolean finished = false;
+
+	// CONSTRUCTOR
 	public TfIdf_Indexer(ArrayList<FileFather> pFileList) {
-		super();
 		FileList = pFileList;
-		WordSet =  WordSetMaker(); 
-		Idfdict = IdfDictMaker(); 
-		Index = new Object[FileList.size()][WordSet.size()+1];
-		Index = indexer();
 	}
-	public Object[][] getIndex(){
+
+	public Object[][] getIndex() {
 		return Index;
 	}
 
 	public ArrayList<FileFather> getFileList() {
 		return FileList;
 	}
+
 	public void setFileList(ArrayList<FileFather> fileList) {
 		FileList = fileList;
 	}
+
 	public Map<String, Double> getIdfdict() {
 		return Idfdict;
 	}
+
 	public void setIdfdict(Map<String, Double> idfdict) {
 		Idfdict = idfdict;
 	}
+
 	public Set<String> getWordSet() {
 		return WordSet;
 	}
+
 	public void setWordSet(Set<String> wordSet) {
 		WordSet = wordSet;
 	}
-	public Map<String, Double> IdfDictMaker() {
-		 for (String actualWord : WordSet) {
-			 Idfdict.put(actualWord, (double) 0);
-		 }
-		return Idfdict;
+
+	public void IdfDictMaker() {
+		for (String actualWord : this.WordSet) {
+			this.Idfdict.put(actualWord, (double) 0);
+		}
 	}
-	
-	public Set<String> WordSetMaker() {
-		 for (FileFather file:FileList) {
-			 Map<String, Double> dict = file.getDict();
-			 for (Map.Entry<String, Double> entry: dict.entrySet()) {
-				 String word = entry.getKey();
-				 if (!(WordSet.contains(word))) {
-					 WordSet.add(word);
-				 }
-					 
-				 
-			 }
-		 }
-		return WordSet;
-		 
-	}
-	
-	
-	public void TfCalculator() {
-		for (FileFather file: FileList) {
+
+	public void WordSetMaker() {
+		for (FileFather file : this.FileList) {
 			Map<String, Double> dict = file.getDict();
-			for (Map.Entry<String, Double> doc: dict.entrySet()) {
-				doc.setValue(doc.getValue()/file.getAmount());
+			for (Map.Entry<String, Double> entry : dict.entrySet()) {
+				String word = entry.getKey();
+				if (!(this.WordSet.contains(word))) {
+					this.WordSet.add(word);
+				}
+			}
+		}
+
+	}
+
+	public void TfCalculator(Map<String, Double> pDict, int WordAmount) {
+		for (Map.Entry<String, Double> doc : pDict.entrySet()) {
+			doc.setValue(doc.getValue() / WordAmount);
+		}
+	}
+
+	public void idfDict() {
+		for (FileFather file : FileList) {
+			Map<String, Double> dict = file.getDict();
+			for (Map.Entry<String, Double> doc : dict.entrySet()) {
+				String word = doc.getKey();
+				Idfdict.put(word, (double) +1);
+
 			}
 		}
 	}
-	
-	public void idfDict() {
-		for (FileFather file: FileList) {
-			Map<String, Double> dict = file.getDict();
-			for (Map.Entry<String, Double> doc: dict.entrySet()) {
-				String word = doc.getKey();	
-				         Idfdict.put(word,(double) +1);
-				
-			}	
+
+	public void IdfCalculator() {
+		int CantFileFather = FileList.size();
+		for (Map.Entry<String, Double> entry : Idfdict.entrySet()) {
+			entry.setValue(Math.log(CantFileFather / entry.getValue()));
 		}
 	}
-	
-public void IdfCalculator() {
-	int CantFileFather = FileList.size(); 
-	for (Map.Entry<String, Double> entry: Idfdict.entrySet()) {
-		entry.setValue(Math.log(CantFileFather/entry.getValue()));
-	}
-}
 
-public void TfIdfCalculator() {
-	for (FileFather file: FileList) {
-		Map<String, Double> dict = file.getDict();
-		for (Map.Entry<String, Double> doc: dict.entrySet()) {
-			String word= doc.getKey();
-			doc.setValue(doc.getValue()*Idfdict.get(word));
+	public void TfIdfCalculator(Map<String, Double> pDict) {
+		for (Map.Entry<String, Double> doc : pDict.entrySet()) {
+			String word = doc.getKey();
+			if (Idfdict.containsKey(word)) {
+				doc.setValue(doc.getValue() * Idfdict.get(word));
+			}
 		}
 	}
-}
 
-public  Object[] RoadMaker(FileFather pFile) {
-	Map<String, Double> dict = pFile.getDict();
-	Object road[] = new Object[WordSet.size()+1];
-	road[0]= pFile.getPath();
-	 for (String actualWord : WordSet) {
-		 for(int i=1; i<WordSet.size();i++) {
-			 if(dict.containsKey(actualWord)) {
-				 road[i]=dict.get(actualWord);
-			 }else road[i]=0;
-		 }
-	 }
-	 return road;
+	public Double[] RoadMaker(FileFather pFile) {
+		Map<String, Double> dict = pFile.getDict();
+		Double road[] = new Double[WordSet.size()];
+		int position = 0;
+		for (String actualWord : WordSet) {
+			if (dict.containsKey(actualWord)) {
+				road[position] = dict.get(actualWord);
+				position++;
+			} else {
+				road[position] = (double) 0;
+				position++;
+			}
+		}
+		return road;
 	}
 
-
-//@Override
-public Object[][] indexer() {
-	for (FileFather file: FileList) {
-		for(int i=0; i<FileList.size();i++) {
-			Index[i]=RoadMaker(file);
+	public void Calculations() {
+		for (FileFather actualfile : FileList) {
+			Map<String, Double> dict = actualfile.getDict();
+			TfCalculator(dict, actualfile.getAmount());
+			TfIdfCalculator(dict);
 		}
 	}
-	return Index;
-		 
+
+	public void run() {
+		WordSetMaker();
+		IdfDictMaker();
+		idfDict();
+		IdfCalculator();
+		Calculations();
+		Index = new Double[FileList.size()][WordSet.size()];
+		Index = indexer();
+		finished = true;
 	}
-		
-		
-	
-	
+
+	public Boolean isFinished() {
+		return finished;
+	}
+
+	public Double[][] indexer() {
+		Double[][] index = new Double[FileList.size()][WordSet.size()];
+		int position = 0;
+		for (FileFather file : FileList) {
+			index[position] = RoadMaker(file);
+			position++;
+		}
+		return index;
+
+	}
 
 }
