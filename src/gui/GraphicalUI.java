8cbf279416bdce251db881dee717d89d5498536b
Unknown <maria2000jose.mj@gmail.com>
2018-09-19 00:15:55 -0600

    Implemented Singleton in TfIdf_Indexer

--- src/gui/GraphicalUI.java
+++ src/gui/GraphicalUI.java
@@ -36,7 +36,8 @@ public class GraphicalUI extends JFrame implements IGuiConsts {
 		initComponents();
 		this.setVisible(true);
 		this.DL = new DirectoryLoader(fp.getFilePath());
-		this.TFIDF = new TfIdf_Indexer(this.DL.getArr());
+		this.TFIDF = TfIdf_Indexer.getInstance(this.DL.getArr()); 
+				//new TfIdf_Indexer(this.DL.getArr()); NO SE NECESITA PORQUE ES SINGLETON 
 		Object[][] as = TFIDF.getIndex();
 		for(int i = 0; i<as.length; i++) {
 			for(int j = 0; j<as[i].length; j++) {
